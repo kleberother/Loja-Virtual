750c233f15115747b120fc80889be08dae8c63df
kleberoliveira <kleber.nova@gmail.com>
2012-10-18 09:00:10 -0300

    Atualizando download de pastas

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev.lojavirtual.com.br/admin/');
+define('HTTP_CATALOG', 'http://dev.lojavirtual.com.br/');
+define('HTTP_IMAGE', 'http://dev.lojavirtual.com.br/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev.lojavirtual.com.br/admin/');
+define('HTTPS_CATALOG', 'http://dev.lojavirtual.com.br/');
+define('HTTPS_IMAGE', 'http://dev.lojavirtual.com.br/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/system/logs/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/Loja-Virtual/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'loja-omdi');
+define('DB_PREFIX', '');
+?>
