e66e931644ed6d97b189bc5816a2ff962e08bc4e
kleberoliveira <kleber.nova@gmail.com>
2012-10-18 16:01:01 -0300

    Wholesale grid
    Grade de venda de estoque

---
+++ admin/language/english/module/wholesalegrid.php
@@ -0,0 +1,5 @@
+<?php
+$_['entry_option_store']    = 'Type Store';
+$_['text_retail'] = 'Retail';
+$_['text_wholesale'] = 'Wholesale';
+?>
---
+++ admin/language/portuguese-br/module/wholesalegrid.php
@@ -0,0 +1,5 @@
+<?php
+$_['entry_option_store']    = 'Tipo de Loja';
+$_['text_retail'] = 'Varejo';
+$_['text_wholesale'] = 'Atacado';
+?>
