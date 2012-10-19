d06451a41fc48e412532984b43f7facc1ac91ff7
kleberoliveira <kleber.nova@gmail.com>
2012-10-19 11:46:47 -0300

    limite de venda de quantidade e valor

---
+++ admin/language/english/module/limitsales.php
@@ -0,0 +1,4 @@
+<?php
+$_['entry_minimum_amount'] = 'Minimum Quantity for sale: <br /> <span class="help"> Minimum order for an application to be made. <br /> (Use 0 for not having minimum amount.) </ span>';
+$_['entry_minimum_value'] = 'Minimum value for sale: <br /> <span class="help"> Minimum value for an application to be made. <br /> (Use 0 for not having minimum value.) </ span>';
+?>
