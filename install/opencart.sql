b78ee1822fdc080f2a3729074129b82edb52c1b1
Kleber Oliveira <kleber.nova@gmail.com>
2012-10-18 21:02:56 -0300

    remover tabela attribute do sql de instalação

--- install/opencart.sql
+++ install/opencart.sql
@@ -128,23 +128,6 @@ CREATE TABLE IF NOT EXISTS `oc_attribute` (
   PRIMARY KEY (`attribute_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=12 ;
 
---
--- Extraindo dados da tabela `oc_attribute`
---
-
-INSERT INTO `oc_attribute` (`attribute_id`, `attribute_group_id`, `sort_order`) VALUES
-(1, 6, 1),
-(2, 6, 5),
-(3, 6, 3),
-(4, 3, 1),
-(5, 3, 2),
-(6, 3, 3),
-(7, 3, 4),
-(8, 3, 5),
-(9, 3, 6),
-(10, 3, 7),
-(11, 3, 8);
-
 -- --------------------------------------------------------
 
 --
