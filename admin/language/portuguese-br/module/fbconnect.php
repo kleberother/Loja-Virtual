9ed90612d1ff0e064041b372aef7228eb520272d
kleberoliveira <kleber.nova@gmail.com>
2012-10-25 10:34:48 -0200

    tradução facebook connect

---
+++ admin/language/portuguese-br/module/fbconnect.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']       = 'Facebook Connect';
+
+// Text
+$_['text_module']         = 'Modulos';
+$_['text_success']        = 'Sucesso: Você modificou módulo Facebook Connect!';
+$_['text_content_top']    = 'Topo do Conteúdo';
+$_['text_content_bottom'] = 'Rodapé do Conteúdo';
+$_['text_column_left']    = 'Coluna da Esquerda';
+$_['text_column_right']   = 'Coluna da Direita';
+
+// Entry
+$_['entry_creator']      = '';
+$_['entry_updated']      = '';
+$_['entry_version']      = '';
+$_['entry_licence']      = '';
+$_['entry_apikey']        = 'Facebook API Key:';
+$_['entry_apisecret']     = 'Facebook API Secret:';
+$_['entry_pwdsecret']     = 'Chave Criptografia de senha:';
+$_['entry_pwdsecret_desc']= 'Para tornar a senha mais segura do cliente, de 4 a 8 caracteres (por exemplo w7x4).';
+$_['entry_button']	  = 'O botão Conectar:';
+$_['entry_button_desc']	  = 'Pode ser um texto ou código html.';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Posição:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Ordem:';
+
+// Error
+$_['error_permission']    = 'Aviso: Você não tem permissão para modificar o módulo mais vistos!';
+$_['error_code']         = 'Erro: Facebook API Key, Facebook API Secreto e da senha da chave de criptografia necessária!';
+?>
