ae26e11de205b83e068ac3204b4d987261fdbb69
kleberoliveira <kleber.nova@gmail.com>
2012-12-03 13:29:43 -0200

    recursos a mais
    - Compra após o login
    - Correção de segurança
    - Correção de frete de correios

---
+++ catalog/language/english/module/afterlogin.php
@@ -0,0 +1,3 @@
+<?php
+$_['entry_after_login'] = 'Access your <a href="%s">account</a> or <a href="%s">create an account</a> to make purchase.';
+?>
---
+++ catalog/language/portuguese-br/module/afterlogin.php
@@ -0,0 +1,3 @@
+<?php
+$_['entry_after_login'] = 'Acesse sua <a href="%s">conta</a> ou <a href="%s">cadastre-se</a> para efetuar compra.';
+?>
