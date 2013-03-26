6472e93015f947f9b70714a608f5e472eb8bf331
Kleber Oliveira <kleber.oliveira@live.com>
2013-03-26 09:57:38 -0300

    Alteração no sql para inclusão de dados newsletters

--- install/opencart.sql
+++ install/opencart.sql
@@ -2,7 +2,7 @@
 -- version 3.2.4
 -- http://www.phpmyadmin.net
 --
--- Máquina: localhost
+-- Máquina: Kleber
 -- Data de Criação: 26-Mar-2013 às 12:38
 -- Versão do servidor: 5.1.44
 -- versão do PHP: 5.3.1
